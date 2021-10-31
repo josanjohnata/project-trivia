152d42db31a9bdd9a44fd5f26f28f592baf4d982
Josan Johnata <josanjohnata@gmail.com>
2021-10-30 21:39:30 -0300

    Updates

---
+++ precache-manifest.eb662ea79513ece086aa9422c8b11cb0.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce9ac55e2494a7e7f376581a89827c6",
+    "url": "/project-trivia/index.html"
+  },
+  {
+    "revision": "3b528192d7dde338c8b9",
+    "url": "/project-trivia/static/css/main.dd633bed.chunk.css"
+  },
+  {
+    "revision": "32ac70020deaa46f61ec",
+    "url": "/project-trivia/static/js/2.4037193f.chunk.js"
+  },
+  {
+    "revision": "e532ddfa4cdc43666cb87ccb15f6fafc",
+    "url": "/project-trivia/static/js/2.4037193f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b528192d7dde338c8b9",
+    "url": "/project-trivia/static/js/main.7dd8268b.chunk.js"
+  },
+  {
+    "revision": "569568925bf22a0b7555",
+    "url": "/project-trivia/static/js/runtime-main.637fee17.js"
+  },
+  {
+    "revision": "4ba5d3939bd1190737634a45bc9868ac",
+    "url": "/project-trivia/static/media/Errou.4ba5d393.mp3"
+  },
+  {
+    "revision": "2bb4c1b088a6145b8a58320e84cb428d",
+    "url": "/project-trivia/static/media/ShowDoMilhaoTheme.2bb4c1b0.mp3"
+  },
+  {
+    "revision": "61586ca632132ab3cc130f371db94da5",
+    "url": "/project-trivia/static/media/about.61586ca6.png"
+  },
+  {
+    "revision": "10a134b1678b47a79660bbab8f151bd7",
+    "url": "/project-trivia/static/media/back_1.10a134b1.png"
+  },
+  {
+    "revision": "943b8a142f07504e973955e92752bdd5",
+    "url": "/project-trivia/static/media/back_3.943b8a14.png"
+  },
+  {
+    "revision": "ec5c449fcba8db6a96e07979cc6dc5c0",
+    "url": "/project-trivia/static/media/back_4.ec5c449f.png"
+  },
+  {
+    "revision": "ea46dc6b392e5370fdb4f7a9f67be4bb",
+    "url": "/project-trivia/static/media/background_login.ea46dc6b.png"
+  },
+  {
+    "revision": "8f1e336b63d7da609391ccb24438473c",
+    "url": "/project-trivia/static/media/certaResposta.8f1e336b.mp3"
+  },
+  {
+    "revision": "bad035b2b14ccaec64693c723e6bb506",
+    "url": "/project-trivia/static/media/gear.bad035b2.png"
+  },
+  {
+    "revision": "c84b8e19d84dda412fc467ba3cf73a2f",
+    "url": "/project-trivia/static/media/header_base.c84b8e19.png"
+  },
+  {
+    "revision": "99df652cd63d2699eac296627f9bf210",
+    "url": "/project-trivia/static/media/proximaPergunta.99df652c.mp3"
+  },
+  {
+    "revision": "d696b7bd73117f4ee0b408084a62df1d",
+    "url": "/project-trivia/static/media/quots_1.d696b7bd.png"
+  },
+  {
+    "revision": "81fd41c1b852246f8ce4240ce2814a05",
+    "url": "/project-trivia/static/media/quots_2.81fd41c1.png"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/project-trivia/static/media/trivia.466d153e.png"
+  },
+  {
+    "revision": "b133f3ea493273e840973fae075805a8",
+    "url": "/project-trivia/static/media/trophy_1.b133f3ea.png"
+  }
+]);
